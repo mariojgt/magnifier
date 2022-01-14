e17a5d616db8138a7564bc7ff9a515de1b899814
Mario Jose Goes Tarosso <mariojgt2@gmail.com>
2022-01-14 15:29:08 +0000

    Update media.php

--- Publish/Config/media.php
+++ Publish/Config/media.php
@@ -20,8 +20,57 @@ return [
         'thumbnail' => [
             'width'  => 150,
             'height' => 300,
+            //'crop'   => true
         ]
     ],
-    'magnifier_middleware' => ['web'],  // Add you guard here web by default
-    'allowed_extensions'   => 'csv,txt,xlx,xls,pdf,jpeg,png,gif,webp',
+
+    'extensions' => [
+        'jpg' => [
+            'resizable' => true,
+            'ctype' => 'image/jpeg',
+        ],
+        'png' => [
+            'resizable' => true,
+            'ctype' => 'image/png',
+        ],
+        'gif' => [
+            'resizable' => true,
+            'ctype' => 'image/gif',
+        ],
+        'jpeg' => [
+            'resizable' => true,
+            'ctype' => 'image/jpeg',
+        ],
+        'bmp' => [
+            'ctype' => 'image/bmp'
+        ],
+        'pdf' => [
+            'ctype' => 'application/pdf'
+        ],
+        'mp4' => [
+            'folder'    => 'expire/',
+            'ctype'     => 'video/mp4'
+        ],
+        'mov' => [
+            'folder'    => 'expire/',
+            'ctype'     => 'video/quicktime'
+        ],
+        'mpg' => [
+            'folder'    => 'expire/',
+            'ctype'     => 'video/mpeg'
+        ],
+        'avi' => [
+            'folder'    => 'expire/',
+            'ctype'     => 'video/x-msvideo'
+        ]
+    ],
+    'use_webp'       => true,
+    'disk'           => 'public',
+    'public_path'    => storage_path(),
+    'default_folder' => 'app/public/media/',
+    'default_size'   => 'small',
+    'img_fall_back'  => '/noimage.jpg', // path is images/imagename.png so the helper knows
+    'no_image_id'    => '1',
+    'max_size'       => '10048',
+    'allowed'        => 'csv,txt,xlx,xls,pdf,jpeg,png,gif,webp',
 ];
